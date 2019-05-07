6b00a8161ddc50207ac68fe416b44bd8ed08a741
CarmenJara <carjaralv@alum.us.es>
2019-05-07 16:03:28 +0200

    translate with token

--- src/main/java/aiss/model/resource/MicrosoftTranslateResource.java
+++ src/main/java/aiss/model/resource/MicrosoftTranslateResource.java
@@ -25,7 +25,8 @@ public class MicrosoftTranslateResource {
 
     private final String access_token;
     private final String uri = "https://api.cognitive.microsofttranslator.com/translate?api-version=3.0&to=";
-   
+	private final String uri0="https://api.cognitive.microsoft.com/sts/v1.0/issuetoken?Subscription-Key=";
+	private final String secret="4ab65da9248a4429af8ec5eb91d16444";
 
     public MicrosoftTranslateResource(String access_token) {
         this.access_token = access_token;
@@ -36,45 +37,54 @@ public class MicrosoftTranslateResource {
      *
      */
     public List<String> getTranslation(List<String> tags, String lang) {
-        ClientResource cr = null;
-        List<String> translated = new ArrayList<>();
-        try {
-            cr = new ClientResource(uri  + lang);
-            log.info("uri-> " + uri  + lang);
-//            Series<Header> headers = (Series<Header>) cr.getRequestAttributes().get("org.restlet.http.headers"); 
-//            if (headers == null) { 
-//            	 log.info("headers is null " );
-//            	headers = new Series<Header>(Header.class); 
-//            	} 
-//            log.info("Bearer " +access_token);
-//            headers.add("Authorization", "Bearer " +access_token );
-            ChallengeResponse chr = new ChallengeResponse(
-                    ChallengeScheme.HTTP_OAUTH_BEARER);
-       chr.setRawValue(access_token);
-       cr.setChallengeResponse(chr);
-      
-            //String result = cr.get(String.class);
-            
-  
-            TranslateRequest[] mtr=new TranslateRequest[tags.size()];
-            
-            for(int i=0; i<tags.size();i++) {
-                mtr[i]=new TranslateRequest();
-                mtr[i].setText(tags.get(i));
-            }
 
-            Translated[] translations=cr.post(mtr, Translated[].class);
-            log.info("translation response " + cr.getResponse().getStatus());
-            for(int i=0; i<tags.size();i++) {
-            	translated.add(translations[i].getTranslations().get(0).getText());
-            	log.info(">>>>>>>>>>>>>>" + translated.get(i));
-            }
-          
-        } catch (ResourceException re) {
-            log.warning("Error translating " + cr.getResponse().getStatus());
-        }
+		String access_token="";		
+		ClientResource cr0 = null;
+		cr0 = new ClientResource(uri0  + secret);
+		log.info(uri0  + secret);
+		cr0.setRequestEntityBuffering(true);
+		access_token = cr0.post(" ",String.class);
+		  log.info("token__________________ "+ access_token);
+		 if (access_token != null && !"".equals(access_token)) {
+			   ClientResource cr = null;
+		        List<String> translated = new ArrayList<>();
+		        try {
+		            cr = new ClientResource(uri  + lang);
+		            log.info("uri-> " + uri  + lang);
 
-        return translated;//TODO
+		            ChallengeResponse chr = new ChallengeResponse(
+		                    ChallengeScheme.HTTP_OAUTH_BEARER);
+		       chr.setRawValue(access_token);
+		       cr.setChallengeResponse(chr);
+		      
+		            //String result = cr.get(String.class);
+		            
+		  
+		            TranslateRequest[] mtr=new TranslateRequest[tags.size()];
+		            
+		            for(int i=0; i<tags.size();i++) {
+		                mtr[i]=new TranslateRequest();
+		                mtr[i].setText(tags.get(i));
+		            }
+
+		            Translated[] translations=cr.post(mtr, Translated[].class);
+		            log.info("translation response " + cr.getResponse().getStatus());
+		            for(int i=0; i<tags.size();i++) {
+		            	translated.add(translations[i].getTranslations().get(0).getText());
+		            	log.info(">>>>>>>>>>>>>>" + translated.get(i));
+		            }
+		          
+		        } catch (ResourceException re) {
+		            log.warning("Error translating " + cr.getResponse().getStatus());
+		        }
+
+		        return translated;
+		 }
+		 else {
+			 log.info("error trying to access Microsoft Translate Token");
+			 return null;
+		 }
+     
 
     }
 
